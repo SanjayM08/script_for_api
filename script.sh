9e47d0cfb831aa40fd2b7c7fc94adc128f5eb8ad
root <root@ip-172-31-18-38.ec2.internal>
2024-07-10 11:55:54 +0000

    Script

---
+++ script.sh
@@ -0,0 +1,85 @@
+clear
+apt update
+clear
+ls
+git clone https://github.com/SanjayM08/docker-fastapi-test.git
+cd docker-fastapi-test
+apt install docker.io -y
+apt install python3
+pip install fastapi
+apt install python3-pip -y
+pip install fastapi 
+virtualenv venv
+apt install python3-virtualenv -y
+virtualenv venv
+ls
+clear
+ls
+ls -la
+cd venv/
+ls
+cd ..
+rm venv/
+rm -rf venv/
+virtualenv venv
+ls
+pip install fastapi
+pip install fastapi uvicorn
+clear
+pip install fastapi
+cd venv/
+cd ..
+rm -rf venv/
+ls
+clear
+apt install python3.12-venv
+python3 -m venv myenv
+ls
+cd myenv/
+ls
+cd bin
+ls
+activate
+cd ..
+myenv\Scripts\activate
+clear
+ls
+cd myenv/
+ls
+cd bin/
+ls
+activate
+cd ../..
+ls
+source myenv/bin/activate
+pip install fastapi
+pip install fastapi uvicorn
+ls
+clear
+apt install docker-compose -y
+docker-compose --build .
+docker-compose --build
+docker-compose . --build
+docker-compose build
+sudo systemctl start docker
+docker --version
+docker info
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.18.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+python3 -m venv myenv
+source myenv/bin/activate
+pip install docker-compose
+pip install cython
+sudo apt-get update
+sudo apt-get install build-essential python3-dev
+pip install --no-cache-dir PyYAML
+pip install PyYAML==5.4.1
+deactivate
+rm -rf myenv
+python3 -m venv myenv
+source myenv/bin/activate
+pip install fastapi
+docker-compose build
+docker-compose up
+deactivate
+
